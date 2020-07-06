7babb3abb7dd6ccb0ad394fc3f542208ee75fe42
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-07-06 08:41:09 -0500

    api routes for updating decks and cards

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,21 +3,39 @@ const User = require('../../models/User');
 
 module.exports = (app) => {
 	// Get all users
-	app.get('/api/users', (req, res) => {
+	app.get('/users', (req, res) => {
 		User.find()
 			.then((users) => res.json(users))
 			.catch((err) => console.log(err));
 	});
 	// Get one user by id
-	app.get('/api/:id', (req, res) => {
+	app.get('/:id', (req, res) => {
 		User.findById(req.params.id)
 		.then(user => res.json(user))
 		.catch(err => console.log(err));
 	});
 
-	app.post('/api/users', (req, res) => {
+	app.post('/users', (req, res) => {
 		User.create(req.body)
 		.then(user => res.json(user))
+		.catch(err => console.log(err));
+	});
+
+	app.put('/decks/:id', (req, res) => {
+		User.findOneAndUpdate({ _id: req.params.id }, {decks: req.body})
+		.then(newDeck => res.json(newDeck))
+		.catch(err => console.log(err));
+	});
+
+	app.put('/cards/:id', (req, res) => {
+		User.findOneAndUpdate({ _id: req.params.id }, 
+		{
+			decks: 
+			[{
+				cards: req.body
+			}]
+		})
+		.then(newDeck => res.json(newDeck))
 		.catch(err => console.log(err));
 	});
 };
